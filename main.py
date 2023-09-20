889443694527bc24f5cb1e5ea9049df9451845ae
Inksplat58 <124327110+Inksplat58@users.noreply.github.com>
2023-09-20 08:31:04 -0400

    hi

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hi")
+import os
+import time
+while True:
+    name = input('give me your name\n\n>')
+    if name.isalpha():
+        break
+    else:
+        print('please only use letters, try again')
